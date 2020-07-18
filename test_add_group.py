3b562a595409ed129f99454c4dc8d7ab33b6c08a
avnukova <vnukova.anna@gmail.com>
2020-07-18 22:30:05 +0300

    test Firefox + Interpreter

--- test_add_group.py
+++ test_add_group.py
@@ -11,12 +11,14 @@ class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-     
+           
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
@@ -25,29 +27,32 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("anechka")
+        wd.find_element_by_name("group_name").send_keys("anechka1")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("hohohoh")
+        wd.find_element_by_name("group_header").send_keys(u"вапап")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("huhuhu")
+        wd.find_element_by_name("group_footer").send_keys("sdfg")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
     
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
+    @property
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
+        try: self.wd.switch_to.alert()
         except NoAlertPresentException as e: return False
         return True
-        
+
     def tearDown(self):
         self.wd.quit()
-        
+     
 if __name__ == "__main__":
     unittest.main()
