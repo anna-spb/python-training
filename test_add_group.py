8f76e785236cf8b3d8fd901c31069fde3307eb2d
anna-spb <48554400+anna-spb@users.noreply.github.com>
2020-07-23 18:17:14 +0300

    создан новый файл и исправлены ошибки предыдущего файла

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import pytest
 from Group import Group
 from application import Application
@@ -12,12 +11,13 @@ def app(request):
     return fixture
 
 
-def test_add_group(self):
+def test_add_group(app):
     app.login(password="secret", username="admin")
     app.Create_group(Group(name="anechka1", header="вапап", footer="sdfg"))
     app.logout()
 
-def test_add_empty_group(self):
+
+def test_add_empty_group(app):
     app.login(password="secret", username="admin")
     app.Create_group(Group(name="", header="", footer=""))
     app.logout()
