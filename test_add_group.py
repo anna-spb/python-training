2b4b8400cada1fc8b095aa462665c7237c97ee17
anna-spb <48554400+anna-spb@users.noreply.github.com>
2020-07-23 16:47:46 +0300

    исправлена ошибка

--- test_add_group.py
+++ test_add_group.py
@@ -1,39 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+
+import pytest
 from Group import Group
 from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_group(self):
-        self.app.login(password="secret", username="admin")
-        self.app.Create_group(Group(name="anechka1", header="вапап", footer="sdfg"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-        self.app.login(password="secret", username="admin")
-        self.app.Create_group(Group(name="", header="", footer=""))
-        self.app.logout()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
+def test_add_group(self):
+    app.login(password="secret", username="admin")
+    app.Create_group(Group(name="anechka1", header="вапап", footer="sdfg"))
+    app.logout()
 
-    #def is_element_present(self, how, what):
-        #try: self.wd.find_element(by=how, value=what)
-        #except NoSuchElementException as e: return False
-        #return True
+def test_add_empty_group(self):
+    app.login(password="secret", username="admin")
+    app.Create_group(Group(name="", header="", footer=""))
+    app.logout()
 
-    #@property
-    #def is_alert_present(self):
-        #try: self.wd.switch_to.alert()
-        #except NoAlertPresentException as e: return False
-        #return True
 
-    def tearDown(self):
-        self.app.destroy()
-     
-if __name__ == "__main__":
-    unittest.main()
