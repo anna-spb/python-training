ea8b9819d9f29c1e3767815c3c2217b53cf32ad7
anna-spb <48554400+anna-spb@users.noreply.github.com>
2020-07-23 21:39:16 +0300

    созданы пакеты для разных зон ответст-ти для контакта

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -12,13 +12,13 @@ def app(request):
 
 
 def test_create_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Group(firstname="hhhhh", middlename="dddd", lastname="ddddd", nickname="ddddd",
                             title="dddd", company= "dddd", address="ggg", home="hhhhhh", mobile="jjjj", work="kkkk",
                             fax="llllll", email="aass@hhj.ru", email2="gggg@kkk.com", email3="rrrr@hh.bu",
                             homepage="http//hjhkjhj.com", bday="18",
                             bmonth="November", byear="3333", aday="18", amonth="October", ayear="2222",
                             address2="hhhhhh", phone2="lllllll", notes="ghghg"))
-    app.logout()
+    app.session.logout()
 
 
