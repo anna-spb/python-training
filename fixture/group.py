fdc488311f366d46c575ce8148da13cc072c3809
anna-spb <48554400+anna-spb@users.noreply.github.com>
2020-07-30 22:22:53 +0300

    созданы разные методы изменения групп

--- fixture/group.py
+++ fixture/group.py
@@ -7,6 +7,26 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def select_first_group(self):
+        wd = self.app.wd
+        # select group
+        wd.find_element_by_name("selected[]").click()
+
+
+
     def create(self, group):
         wd = self.app.wd
         self.open_group_page()
@@ -18,37 +38,35 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def fill_group_form(self, group):
-        wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
-        # select group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_first_group()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        # submit update
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
+
+
     def change_first_group(self, group):
         wd = self.app.wd
         self.open_group_page()
-        # select group
-        wd.find_element_by_name("selected[]").click()
-        # edit
+        self.select_first_group()
         wd.find_element_by_name("edit").click()
-        #edit group
         self.fill_group_form(group)
-        # submit update
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
 
--- test/test_change_group.py
+++ test/test_change_group.py
@@ -6,3 +6,6 @@ from model.group import Group
 def test_change_first_group(app):
     app.group.change_first_group(Group(name="ddd", header="ggg", footer="asd"))
 
+def test_modify_first_group_name(app):
+    app.group.modify_first_group(Group(name="New name"))
+
