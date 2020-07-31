684dc6fe2f9075361d519aac9c31aab1c1008a16
anna-spb <48554400+anna-spb@users.noreply.github.com>
2020-07-31 19:13:57 +0300

    добавлена проверка открытия нужной стр в группах и контактах

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,7 +8,8 @@ class ContactHelper:
 
     def return_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswitch("/addressbook/") and len( wd.find_elements_by_link_text("add new")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def select_first_contact(self):
         wd = self.app.wd
